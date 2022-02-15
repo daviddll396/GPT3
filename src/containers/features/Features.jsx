07d14b3eaa9f3c373970a1295607bcecaa4bdd11
David Dll <61070052+daviddll396@users.noreply.github.com>
2022-02-15 17:30:25 +0100

    Add files via upload

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Feature from '../../components/feature/Feature';
+import './features.css';
+
+const featuresData = [
+  {
+    title: 'Improving end distrusts instantly',
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, eligendi",
+  },
+  {
+    title: 'Become the tended active',
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, eligendi",
+  },
+  {
+    title: 'Message or am nothing',
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, eligendi",
+  },
+  {
+    title: 'Really boy law county',
+    text: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, eligendi",
+  },
+];
+
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Features;
